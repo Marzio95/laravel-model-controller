cd394020fb095ae38f4375914957484531a1c4c2
Marzio95 <marziodellarocca14@gmail.com>
2022-05-04 16:42:37 +0200

    inserisco i dati nel env, collego il database tramite il controller al web e alla route

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+
+class PageController extends Controller
+{
+    public function listMovie()
+    {
+        $myMovies = Movie::all();
+        return view('moviepage', $myMovies);
+    }
+}
