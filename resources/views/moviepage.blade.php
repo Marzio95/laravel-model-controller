f513b25845c0fbfa571cb75e96efffd8f17c84e9
Marzio95 <marziodellarocca14@gmail.com>
2022-05-04 17:43:37 +0200

    inserisco scss

--- resources/views/moviepage.blade.php
+++ resources/views/moviepage.blade.php
@@ -3,14 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">  
+
+    <link rel="stylesheet" href="css/app.css">
     <title>Movies Controller</title>
 </head>
 <body>
-    {{-- TEMPLATE DEI MOVIE --}}
     <h1>questa è la pagina movie</h1>
+    <div class="body">
+    {{-- TEMPLATE DEI MOVIE --}}
         @foreach ($myMovies as $movie)
+        <div class="card">
             <h2>{{ $movie->title }}</h2> 
+            <h2>{{ $movie->nationality }}</h2> 
+            <h2>{{ $movie->date }}</h2> 
+            <h2>{{ $movie->vote }}</h2>
+        </div>
         @endforeach
+    </div>
 </body>
 </html>
